2c0d535d2ff46e3024797e6aeae3923ac2a5c6ec
MinhWaan <hoquan119@gmail.com>
2025-04-21 08:09:37 +0700

    fix UI

--- notifications-app/styles.ts
+++ notifications-app/styles.ts
@@ -1,6 +1,5 @@
 import { StyleSheet, Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
-import { Ionicons } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
     container: {
